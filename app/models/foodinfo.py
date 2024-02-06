f4cfa008242f643952410a7429f2129af53da435
Gek <gakidogeck@gmail.com>
2024-02-05 16:50:07 -0800

    added add_prefix_for_prod

--- app/models/foodinfo.py
+++ app/models/foodinfo.py
@@ -14,7 +14,7 @@ class Food(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id', ondelete='CASCADE')), nullable=False)
     name = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.String(255), nullable=False)
