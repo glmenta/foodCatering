b5bd986d45fc8e45b7c7f77cc4e398c8d44b1a38
Gek <gakidogeck@gmail.com>
2024-02-07 11:33:25 -0800

    added food_menu_id column

--- app/models/foodinfo.py
+++ app/models/foodinfo.py
@@ -52,8 +52,8 @@ class FoodMenu(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
 
-    day = db.relationship('Day', back_populates='food_menus', foreign_keys=[day_id])
-
+    day = db.relationship('Day', back_populates='food_menus')
+    # , foreign_keys=[day_id]
     foods = db.relationship('Food', secondary='food_menu_foods', back_populates='food_menus')
     food_orders = db.relationship('FoodOrder', back_populates='food_menu')
 
