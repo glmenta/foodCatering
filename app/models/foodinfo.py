8ee1bd5a4c537b7ee20f03c4a66a1ad05ea6798f
Gek <gakidogeck@gmail.com>
2024-02-08 08:06:20 -0800

    testing change limit to 500

--- app/models/foodinfo.py
+++ app/models/foodinfo.py
@@ -111,7 +111,7 @@ class FoodImage(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(500), nullable=False)
     food_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('foods.id')), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
