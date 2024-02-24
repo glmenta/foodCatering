d5739273235f94ed5b6527122f0ff5d0c6ef2d05
Gek <gakidogeck@gmail.com>
2024-02-23 16:55:45 -0800

    message rendering on order page

--- react-app/src/components/OrderDetailModal/index.js
+++ react-app/src/components/OrderDetailModal/index.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 import * as orderActions from "../../store/order";
 // import './orderdetail.css'
 
-function OrderDetailModal({ isOpen, onClose, orderId }) {
+function OrderDetailModal({ isOpen, onClose, orderId, orderMessages }) {
     const dispatch = useDispatch();
     const user_orders = useSelector(state => Object.values(state.order.currentUserOrders))
 
@@ -17,25 +17,31 @@ function OrderDetailModal({ isOpen, onClose, orderId }) {
     console.log('Order: ', order)
 
     const totalPrice = order ? order.foodOrders.reduce((acc, foodOrder) => acc + (foodOrder.quantity * foodOrder.food.price), 0) : 0;
-
+    console.log('orderMessages: ', orderMessages)
     return (
         isOpen &&
         <div className='order-detail-modal'>
             <div className='order-detail-modal-header'>
                 <h2>{order?.order_name}</h2>
                 {order?.foodOrders.map(foodOrder => (
-                    <div className='food-order-detail'>
+                    <div className='food-order-detail' key={foodOrder.id}>
                         <div>Food Name: {foodOrder.food.name}</div>
                         <div>Price: ${foodOrder.food.price}</div>
                         <div>Quantity: {foodOrder.quantity}</div>
                     </div>
                 ))}
+                <div className='order-messages'>
+                    <h3>Order Messages</h3>
+                    {Array.isArray(orderMessages?.messages) && orderMessages.messages.map(message => (
+                        <div key={message.id}>{message.content}</div>
+                    ))}
+                </div>
                 <div>Total Price: ${totalPrice.toFixed(2)}</div>
                 <button onClick={onClose}>X</button>
-
             </div>
         </div>
-    )
+    );
+
 }
 
 export default OrderDetailModal
