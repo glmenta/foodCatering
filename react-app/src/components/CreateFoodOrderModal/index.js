4635b34ac530879f14c41f7e0eebf64fcb85d1d5
Gek <gakidogeck@gmail.com>
2024-03-04 16:55:16 -0800

    started on create order and food order modal

--- react-app/src/components/CreateFoodOrderModal/index.js
+++ react-app/src/components/CreateFoodOrderModal/index.js
@@ -3,23 +3,44 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import * as foodActions from "../../store/food";
 import * as sessionActions from "../../store/session";
+import * as orderActions from "../../store/order";
 
-function CreateFoodOrderModal({ isOpen, onClose }) {
+// if no orders, make a new order; if there is order, add food to order
+function CreateFoodOrderModal({ user_id, menu_id, food, isOpen, onClose }) {
     const dispatch = useDispatch();
     const history = useHistory();
     const [errors, setErrors] = useState([]);
+    const [quantity, setQuantity] = useState(1);
 
-    console.log('inside modal')
+    console.log('inside modal', menu_id, food)
+
+    useEffect(() => {
+        dispatch(orderActions.getUserOrdersThunk(user_id));
+    }, [dispatch, user_id]);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = await dispatch(foodActions.createFoodOrder());
+        let errors = {};
+
+        const payload = {
+            menu_id,
+            food_id: food.id,
+            quantity
+        }
+
+        const data = await dispatch(foodActions.createFoodOrder(payload));
+
         if (data) {
             setErrors(data);
         } else {
+            setErrors([]);
+            setQuantity(1);
             onClose();
         }
+
     };
 
+    // menu_id, food_id, quantity
     return (
         isOpen &&
         <div className='create-food-order-modal'>
@@ -30,6 +51,15 @@ function CreateFoodOrderModal({ isOpen, onClose }) {
                         <li key={idx}>{error}</li>
                     ))}
                 </ul>
+                <h2></h2>
+                <label>
+                    Quantity
+                    <input
+                        type="number"
+                        value={quantity}
+                        onChange={(e) => setQuantity(e.target.value)}
+                    />
+                </label>
                 <button type="submit">Submit</button>
             </form>
         </div>
