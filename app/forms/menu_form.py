946ea996c578cb33b80d2247d6cdd27a3898d915
Gek <gakidogeck@gmail.com>
2024-03-25 15:13:50 -0700

    modified create new menu, add/remove food from menu

--- app/forms/menu_form.py
+++ app/forms/menu_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 from app.models.foodinfo import Food, FoodMenu, FoodOrder
 # from app.models.day import Day
@@ -7,8 +7,9 @@ from flask import current_app
 
 class MenuForm(FlaskForm):
     # Add food to the menu depending on the day
-    day = SelectField('day', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired()])
     food = SelectField('food', validators=[DataRequired()])
+    isActive = BooleanField('isActive')
     submit = SubmitField('Submit')
 
     def __init__(self, *args, **kwargs):
@@ -16,12 +17,19 @@ class MenuForm(FlaskForm):
         self.add_food_to_day()
 
     def add_food_to_day(self):
-        # days = Day.query.all()
-        # foods = Food.query.all()
+        foods = Food.query.all()
+        self.food.choices = [(str(food.id), food.name) for food in foods]
+        print('Choices:', self.food.choices)
 
-        # self.day.choices = [(str(day.id), day.day) for day in days]
-        # self.food.choices = [(str(food.id), food.name) for food in foods]
-        pass
+class UpdateFoodMenuForm(FlaskForm):
+    food = SelectField('food', validators=[DataRequired()])
+    isActive = BooleanField('isActive')
+    submit = SubmitField('Submit')
 
-class AddFoodToMenuForm(FlaskForm):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(UpdateFoodMenuForm, self).__init__(*args, **kwargs)
+        self.add_food_to_day()
+
+    def add_food_to_day(self):
+        foods = Food.query.all()
+        self.food.choices = [(str(food.id), food.name) for food in foods]
