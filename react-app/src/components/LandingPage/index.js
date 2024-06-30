538e0780dd0080d863ee13a81f5d6682665148a7
Gek <gakidogeck@gmail.com>
2024-06-29 21:15:19 -0700

    started on food imgs for landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import { useDispatch, useSelector, useEffect } from "react-redux";
+import * as foodActions from "../../store/food";
 
 export default function LandingPage() {
+    const dispatch = useDispatch();
+    const [setFoodImgs, setSetFoodImgs] = useState([]);
+    useEffect(() => {
+        dispatch(foodActions.getAllFoodsThunk());
+    }, [dispatch]);
     return (
         <div className="landing-page">
             <h1 >Kapampangan's Best</h1>
